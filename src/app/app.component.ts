c47687b4829b1ccf8ceb4cabff1821d4332945ce
adiseshu <9032618121v@gmail.com>
2024-12-17 14:39:18 +0530

    Added some files for the Housing List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {HomeComponent} from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent],
+  template: `
+  <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/16304.png" alt="logo" aria-hidden="true" style="height:100px;width:100px;"/>
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'homes';
 }
